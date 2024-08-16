779f7c457ca1c95aa85341811e05e52b204ed503
shiyuan.su <shiyuan.su@mobvista.com>
2024-08-16 17:01:21 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,13 +3,16 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -71,10 +74,17 @@ public class EmployeeController {
         return Result.success();
     }
     @PostMapping("/employee")
-    public Result createEmployerr(@RequestBody EmployeeDTO employeeDTO){
+    public Result createEmployee(@RequestBody EmployeeDTO employeeDTO){
         log.info("新增员工：{}",employeeDTO);
         employeeService.insertEmployee(employeeDTO);
         return Result.success();
+    }
+    @GetMapping("/page")
+    @ApiOperation("分页查询员工信息")
+    public Result<PageResult> selectEmployee(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("分页查询员工：{}",employeePageQueryDTO);
+        PageResult pageResult = employeeService.selectPage(employeePageQueryDTO);
+        return Result.success(pageResult);
     }
 
 }
